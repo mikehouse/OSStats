99922c3be6c61c9d664d6f3db6bde31fc8077ddd
Mikhail Demidov <mike.house.nsk@gmail.com>
2020-06-26 14:17:49 +0700

    Move non UI logic off main thread

--- OSStats/AppDelegate.m
+++ OSStats/AppDelegate.m
@@ -12,12 +12,23 @@ NSTimeInterval updateInterval = 5.0;
 
 @property(nonatomic) NSStatusItem *statusItem;
 @property(nonatomic) NSTimer *updateTimer;
+@property(nonatomic) NSOperationQueue *operationQueue;
+@property(nonatomic) dispatch_queue_t serial_background_queue;
 
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    dispatch_queue_t dispatchQueueCreate = dispatch_queue_create("os.stats.queue.serial", DISPATCH_QUEUE_SERIAL);
+
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    queue.maxConcurrentOperationCount = 1;
+    queue.underlyingQueue = dispatchQueueCreate;
+
+    self.operationQueue = queue;
+    self.serial_background_queue = dispatchQueueCreate;
+
     NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
     NSStatusItem *statusItem = [statusBar statusItemWithLength:-1];
     NSStatusBarButton *barButton = statusItem.button;
@@ -41,22 +52,28 @@ NSTimeInterval updateInterval = 5.0;
 }
 
 - (void)updateStatusBar {
-    OsStats *stats = os_stats();
-    if (stats != NULL) {
-        NSString *process = nil;
-        if (stats->cpu_temperature > 0.0) {
-            process = [NSString stringWithFormat:@"cpu %0.1lf °C %s%% %s", stats->cpu_temperature, stats->max_consume_proc_value, stats->max_consume_proc_name];
-        } else {
-            process = [NSString stringWithFormat:@"cpu %s%% %s", stats->max_consume_proc_value, stats->max_consume_proc_name];
+    NSOperation *operation = [NSBlockOperation blockOperationWithBlock:^{
+        NSAssert(!NSThread.isMainThread, @"");
+        OsStats *stats = os_stats();
+        if (stats != NULL) {
+            NSString *process = nil;
+            if (stats->cpu_temperature > 0.0) {
+                process = [NSString stringWithFormat:@"cpu %0.1lf °C %s%% %s", stats->cpu_temperature, stats->max_consume_proc_value, stats->max_consume_proc_name];
+            } else {
+                process = [NSString stringWithFormat:@"cpu %s%% %s", stats->max_consume_proc_value, stats->max_consume_proc_name];
+            }
+            NSOperatingSystemVersion systemVersion = [NSProcessInfo processInfo].operatingSystemVersion;
+            NSNumber *offset = systemVersion.minorVersion >= 16 ? @(0) : @(-16);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.statusItem.button.attributedTitle = [[NSAttributedString alloc] initWithString:process attributes:@{
+                    NSFontAttributeName: [NSFont systemFontOfSize:12],
+                    NSBaselineOffsetAttributeName: offset // 🤷‍♂️
+                }];
+            });
+            os_stats_free(stats);
         }
-        NSOperatingSystemVersion systemVersion = [NSProcessInfo processInfo].operatingSystemVersion;
-        NSNumber *offset = systemVersion.minorVersion >= 16 ? @(0) : @(-16);
-        self.statusItem.button.attributedTitle = [[NSAttributedString alloc] initWithString:process attributes:@{
-            NSFontAttributeName: [NSFont systemFontOfSize:12],
-            NSBaselineOffsetAttributeName: offset // 🤷‍♂️
-        }];
-        os_stats_free(stats);
-    }
+    }];
+    [self.operationQueue addOperation:operation];
 }
 
 - (void)addMenu {
